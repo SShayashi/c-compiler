71fe1d125a7463f74f178bd5007f234ef674d9eb
hayashi <sshayashi0208@gmail.com>
2020-05-11 23:39:41 +0900

    code生成まで

--- main.c
+++ main.c
@@ -1,14 +1,16 @@
+#include "9cc.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include "9cc.h"
 
 // 現在着目しているトークン
 Token *token;
 
 // 入力プログラム
 char *user_input;
+// 関数名の一時保存先
+static char *funcname;
 
 int is_alnum(char c)
 {
@@ -135,33 +137,44 @@ int main(int argc, char **argv)
     // トークナイズしてパースする
     user_input = argv[1];
     token = tokenize(user_input);
+    Function *prog = program();
+
+    // ローカル変数のoffsetを計算して，各関数ポインタに入れていく
+    for (Function *fn = prog; fn; fn = fn->next)
+    {
+        int offset = 0;
+        for (LVar *var = prog->locals; var; var = var->next)
+        {
+            offset += 8;
+            var->offset = offset;
+        }
+        fn->stack_size = offset;
+    }
 
     // アセンブリの前半部分を出力
     printf(".intel_syntax noprefix\n");
-    printf(".global main\n");
-    printf("main:\n");
 
-    // プロローグ
-    // 変数の領域確保
-    printf("  push rbp\n");
-    printf("  mov rbp, rsp\n");
-    if (locals)
+    //TODO ここで各関数のStacksize計算する
+    for (Function *fn = prog; fn; fn = fn->next)
     {
-        printf("  sub rsp, %d\n", locals->offset);
-    }
-    for (int i = 0; code[i]; i++)
-    {
-        gen(code[i]);
+        printf(".global main\n");
+        printf("%s:\n", fn->name);
+        funcname = fn->name;
 
-        // 式の評価結果としてスタックに一つの値が残っている？ので，スタック溢れをしないようポップ
-        printf("  pop rax\n");
-    }
+        // プロローグ
+        printf("  push rbp\n");
+        printf("  mov rbp, rsp\n");
+        printf("  sub rsp, %d\n", fn->stack_size);
 
-    // エピローグ
-    // 最後の式の結果がRAXに残っているのでそれが返り値になる
-    // RSPがリターンアドレスを指している状態にして，ret命令を呼ぶ必要あり
-    printf("  mov rsp, rbp\n");
-    printf("  pop rbp\n");
-    printf("  ret\n");
+        // アセンブリコードの出力
+        for (Node *node = fn->node; node; node = node->next)
+            gen(node);
+
+        // エピローグ
+        printf(".L.return.%s:\n", funcname);
+        printf("  mov rsp, rbp\n");
+        printf("  pop rbp\n");
+        printf("  ret\n");
+    }
     return 0;
 }
